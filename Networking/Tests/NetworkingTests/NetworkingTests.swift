3ffd8e1996843567c14a705f162096ed3a5bc351
marek wala <marek.wala.97@gmail.com>
2026-07-13 23:19:58 +0200

    Add NetworkFXRepositoryTests

--- Networking/Tests/NetworkingTests/NetworkingTests.swift
+++ Networking/Tests/NetworkingTests/NetworkingTests.swift
@@ -1,12 +1,63 @@
 import XCTest
 @testable import Networking
+import Core
 
-final class NetworkingTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class NetworkFXRepositoryTests: XCTestCase {
+    
+    private var sessionMock: URLSessionMock!
+    private var sut: NetworkFXRepository!
+    
+    override func setUp() {
+        super.setUp()
+        sessionMock = URLSessionMock()
+        sut = NetworkFXRepository(session: sessionMock, baseURL: "https://mock.api.transfergo.com/api/fx-rates")
+    }
+    
+    override func tearDown() {
+        sessionMock = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_fetchFXRate_success_returnsMappedResult() async throws {
+        // Given
+        let json = """
+        {
+            "from": "PLN",
+            "to": "EUR",
+            "rate": 0.25,
+            "fromAmount": 100.0,
+            "toAmount": 25.0
+        }
+        """
+        sessionMock.stubbedData = json.data(using: .utf8)
+        
+        // When
+        let result = try await sut.fetchFXRate(from: "PLN", to: "EUR", amount: 100.0)
+        
+        // Then
+        XCTAssertEqual(result.from, "PLN")
+        XCTAssertEqual(result.to, "EUR")
+        XCTAssertEqual(result.rate, 0.25)
+        XCTAssertEqual(result.fromAmount, 100.0)
+        XCTAssertEqual(result.toAmount, 25.0)
+    }
+    
+    func test_fetchFXRate_serverError_throwsError() async {
+        // Given
+        sessionMock.stubbedResponse = HTTPURLResponse(
+            url: URL(string: "https://mock.api.transfergo.com/api/fx-rates")!,
+            statusCode: 500,
+            httpVersion: nil,
+            headerFields: nil
+        )
+        
+        // Then
+        do {
+            _ = try await sut.fetchFXRate(from: "PLN", to: "EUR", amount: 100.0)
+            XCTFail("Should throw serverError")
+        } catch {
+            XCTAssertEqual(error as? NetworkError, NetworkError.serverError(statusCode: 500))
+        }
     }
 }
