7ab9db44e3b679aa3689a77693216eb09ec35800
marek wala <marek.wala.97@gmail.com>
2026-07-13 20:15:39 +0200

    Inital architecture/modules

---
+++ TransferGo/TransferGo/TransferGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferGoApp.swift
+//  TransferGo
+//
+//  Created by Marek Wala on 13/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TransferGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
