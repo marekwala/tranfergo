0ae32fec58f28fe4b5dbd1143e91237c29be7c1f
marek wala <marek.wala.97@gmail.com>
2026-07-13 21:17:45 +0200

    Add CalculateTransferUseCase -  tests green

---
+++ Core/Sources/Core/Domain/CalculateTransferUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  Untitled.swift
+//  Core
+//
+//  Created by Marek Wala on 13/07/2026.
+//
+
+public protocol CalculateTransferUseCaseProtocol {
+    func execute(sourceAmount: Double, sourceCurrency: Currency, targetCurrency: Currency) async throws -> TransferCalculation
+}
+
+public final class CalculateTransferUseCase: CalculateTransferUseCaseProtocol {
+    private let repository: FXRepository
+    
+    public init(repository: FXRepository) {
+        self.repository = repository
+    }
+    
+    public func execute(sourceAmount: Double, sourceCurrency: Currency, targetCurrency: Currency) async throws -> TransferCalculation {
+        let result = try await repository.fetchFXRate(
+            from: sourceCurrency.code,
+            to: targetCurrency.code,
+            amount: sourceAmount
+        )
+        
+        return TransferCalculation(
+            sourceCurrency: sourceCurrency,
+            targetCurrency: targetCurrency,
+            rate: result.rate,
+            sourceAmount: result.fromAmount,
+            targetAmount: result.toAmount
+        )
+    }
+}
