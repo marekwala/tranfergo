6de541f399cf01334f4438c2f110f33c5d54e7df
marek wala <marek.wala.97@gmail.com>
2026-07-14 10:17:03 +0200

    Delete autocreated unnecessary comments by XCode

--- Networking/Package.swift
+++ Networking/Package.swift
@@ -9,7 +9,6 @@ let package = Package(
             .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Networking",
             targets: ["Networking"]
@@ -19,8 +18,6 @@ let package = Package(
             .package(path: "../Core")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Networking",
             dependencies: [.product(name: "Core", package: "Core")]
