cd0ae6522ca55ec35c75fbb402433a078248e720
marek wala <marek.wala.97@gmail.com>
2026-07-14 11:16:40 +0200

    Add SupportedCurrencies

--- Core/Sources/Core/Models/Currency.swift
+++ Core/Sources/Core/Models/Currency.swift
@@ -5,14 +5,39 @@
 //  Created by Marek Wala on 13/07/2026.
 //
 
-public struct Currency: Equatable, Hashable {
+import Foundation
+
+public struct Currency: Hashable, Identifiable, Equatable, Sendable {
+    public var id: String { code }
     public let code: String
     public let name: String
-    public let symbol: String
+    public let flagImageName: String
+    public let limit: Decimal
     
-    public init(code: String, name: String, symbol: String) {
+    public init(code: String, name: String, flagImageName: String, limit: Decimal) {
         self.code = code
         self.name = name
-        self.symbol = symbol
+        self.flagImageName = flagImageName
+        self.limit = limit
+    }
+    
+    public static func == (lhs: Currency, rhs: Currency) -> Bool {
+        return lhs.code == rhs.code &&
+               lhs.name == rhs.name &&
+               lhs.flagImageName == rhs.flagImageName &&
+               lhs.limit == rhs.limit
+    }
+}
+
+public enum SupportedCurrencies : Sendable{
+    public static let all: [Currency] = [
+        Currency(code: "PLN", name: "Poland", flagImageName: "flag_pl", limit: 20000),
+        Currency(code: "EUR", name: "Germany", flagImageName: "flag_de", limit: 5000),
+        Currency(code: "GBP", name: "Great Britain", flagImageName: "flag_gb", limit: 1000),
+        Currency(code: "UAH", name: "Ukraine", flagImageName: "flag_ua", limit: 50000)
+    ]
+    
+    public static func find(_ code: String) -> Currency? {
+        all.first(where: { $0.code == code })
     }
 }
